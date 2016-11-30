504ad4e48699e4d8c284f8db99f55033244fc549
s802975159 <s802975159@china.ece.uprm.edu>
2016-11-30 14:43:40 -0400

    First part

---
+++ src2/RecursiveExamples.java
@@ -0,0 +1,14 @@
+
+public class RecursiveExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 f( );  
+	}
+	 private static void f( ) { 
+
+         f( ); 
+
+} 
+
+}
